8218b77495e611ddf12dbe950a0ad21a64e8f717
Brecht Omey <brecht@brec.ht>
2017-09-06 10:42:07 +0200

    added ignore rules to at-rule-no-unknown

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ module.exports = {
     'stylelint-order',
   ],
   rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          // additional scss at-rules:
+          'content', 'each', 'else', 'error', 'extend', 'for', 'function', 'if', 'include', 'mixin', 'return',
+        ],
+      },
+    ],
     'order/order': [
       'at-variables',
       'dollar-variables',
