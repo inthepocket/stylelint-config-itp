d7d0a49559cd847cd1d64573e2f88c572efc70c2
Brecht Omey <brecht@brec.ht>
2017-09-06 10:18:43 +0200

    added stylelint-order && stylelint-order rules

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = {
-
+  extends: 'stylelint-config-standard',
+  plugins: [
+    'stylelint-order',
+  ],
+  rules: {
+    'order/order': [
+      'at-variables',
+      'dollar-variables',
+      'custom-properties',
+      'declarations',
+      'rules',
+    ],
+    'order/properties-alphabetical-order': true,
+  },
 };
